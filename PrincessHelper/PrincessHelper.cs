e2c79862a3a5c1e94c954ed332fd668e525758d9
atmos9521 <willy77423@gmail.com>
2020-12-04 09:03:22 +0800

    修正UR_All 腳色順序不正確

--- PrincessHelper/PrincessHelper.cs
+++ PrincessHelper/PrincessHelper.cs
@@ -11,21 +11,20 @@ namespace PrincessHelper
     {
         public PrincessHelper()
         {
-            InitializeComponent();            
+            InitializeComponent();
+            ReadReNewDate();
         }
 
         DateTime StartTime = new DateTime(2020,8,25);
-        DateTime HourStartTime = new DateTime(2020,8,24,1,0,0);
+        DateTime HourStartTime = new DateTime(2020,11,30,1,0,0);
+        DateTime URStartTime = new DateTime(2020,11,26);
         private void Search_btn_ClickAsync(object sender, EventArgs e)
         {
             DateTime SelectDate = DateTime.Parse(dateTimePicker1.Text);
             TimeSpan diffday = SelectDate - StartTime;
             //差的天數
             int diffdays = 0;
-            int.TryParse(diffday.TotalDays.ToString(), out diffdays);
-
-            int diffhours = 0;
-            int.TryParse(diffday.TotalHours.ToString(), out diffhours);
+            int.TryParse(diffday.TotalDays.ToString(), out diffdays);            
 
             //讀檔            
             string R_Roll, SR_Roll, SSR_Roll, UR_Roll, All_Roll;
@@ -34,9 +33,17 @@ namespace PrincessHelper
                 R_Roll = ReturnRoll("../../json/R.json", diffdays);
                 SR_Roll = ReturnRoll("../../json/SR.json", diffdays);
                 SSR_Roll = ReturnRoll("../../json/SSR.json", diffdays);
+
+                diffday = SelectDate - URStartTime;
+                int.TryParse(diffday.TotalDays.ToString(), out diffdays);
                 UR_Roll = ReturnRoll("../../json/UR.json", diffdays);
-                ShowResult_txt.Text = string.Format("R:{0}\r\nSR:{1}\r\nSSR:{2}\r\nUR:{3}"
+                ShowResult_txt.Text = string.Format("R:{0}\r\nSR:{1}\r\nSSR:{2}\r\nUR:{3}\r\n\r\n限時精靈:"
                                             , R_Roll, SR_Roll, SSR_Roll, UR_Roll);
+
+                diffday = SelectDate - HourStartTime;
+                int diffhours = 0;
+                int.TryParse(diffday.TotalHours.ToString(), out diffhours);
+                ReturnAllRoll(diffhours);
             }
             catch (FileNotFoundException ex)
             {
@@ -54,6 +61,34 @@ namespace PrincessHelper
                 RollName = Rolls[diffdays % Rolls.Length];
             }
             return RollName;
+        }
+        void ReturnAllRoll(int diffhours)
+        {
+            string[] RollName = new string[12];
+            using (var sr = new StreamReader("../../json/All.json"))
+            {
+                string json = sr.ReadToEnd();
+                //R_Rolls = JsonConvert.DeserializeObject<List<Rolls>>(Rjson);
+                string[] Rolls = JsonConvert.DeserializeObject<string[]>(json);
+                for (int i = 1; i <= 24; i++)
+                {
+                    ShowResult_txt.Text = string.Format("{0}\r\n{1}:00\t{2}", ShowResult_txt.Text, i, Rolls[(diffhours + i) % Rolls.Length]);                    
+                }
+            }
+        }
+
+        void ReadReNewDate() {
+            try
+            {
+                using (var sr = new StreamReader("../../json/ReNewDate.json")) {
+                    string json = sr.ReadToEnd();
+                    var test = JsonConvert.DeserializeObject(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         Rolls Rolls = new Rolls();
